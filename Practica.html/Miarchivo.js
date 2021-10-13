573eaa38288919c09c4fbe6e25b76d5bb7a797df
DorMariaLuna <79731053+DorMariaLuna@users.noreply.github.com>
2021-10-13 17:25:48 -0500

    13/10/2021
    Siguiendo las practicas

--- Practica.html/Miarchivo.js
+++ Practica.html/Miarchivo.js
@@ -1,5 +1,18 @@
-<script src="Miarchivo.js">Practica</script>
-for ( var i=0; i<=7; i++){
-    //bloque de instrucciones
-   }
+<script src="Miarchivo.js"></script>
+
+    for ( var i=0; i<=4; i++){
+        console.log(7) //7
+       }
+
+       {
+           document.write(hola);
+       }
+       var numero = 1;
+       if (numero > 2) {
+           console.log('el numero es mayor que 2');
+       } else if (numero <2 ) {
+           console.log('el numero es menor que 2');
+      
+       } else{  console.log('el numero es igual que 2');
    
+       }
